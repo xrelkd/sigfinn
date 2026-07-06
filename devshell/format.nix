13cffd4974415c46e53bd1e7963f5369f6f80b37
xrelkd <46590321+xrelkd@users.noreply.github.com>
2026-07-06 19:06:53 +0800

    chore(nix): replace `nixpkgs-fmt` with `nixfmt`

--- devshell/format.nix
+++ devshell/format.nix
@@ -1,29 +1,30 @@
-{ pkgs, }:
+{ pkgs }:
 
 pkgs.runCommandNoCC "check-format"
-{
-  buildInputs = with pkgs; [
-    fd
+  {
+    buildInputs = with pkgs; [
+      fd
 
-    shellcheck
+      shellcheck
 
-    nixpkgs-fmt
-    nodePackages.prettier
-    shfmt
-    taplo
-    treefmt
-  ];
-} ''
-  treefmt \
-    --allow-missing-formatter \
-    --fail-on-change \
-    --no-cache \
-    --formatters prettier \
-    --formatters nix \
-    --formatters shell \
-    --formatters toml \
-    -C ${./..}
+      nixfmt
+      prettier
+      shfmt
+      taplo
+      treefmt
+    ];
+  }
+  ''
+    treefmt \
+      --allow-missing-formatter \
+      --fail-on-change \
+      --no-cache \
+      --formatters prettier \
+      --formatters nix \
+      --formatters shell \
+      --formatters toml \
+      -C ${./..}
 
-  # it worked!
-  touch $out
-''
+    # it worked!
+    touch $out
+  ''
